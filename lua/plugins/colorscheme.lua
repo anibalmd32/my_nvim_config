8a954cb5f4fed2eaa210a18e9e04434edb07d223
Anibal Mendoza <anibalmd32@gmail.com>
2025-06-07 02:17:48 -0400

    add git signs plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,12 @@
 return {
 	"folke/tokyonight.nvim",
+	-- 'olimorris/onedarkpro.nvim',
+	-- 'bluz71/vim-moonfly-colors',
 	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.cmd([[colorscheme tokyonight]])
+		-- vim.cmd([[colorscheme onedark]])
+		-- vim.cmd([[colorscheme moonfly]])
 	end,
 }
