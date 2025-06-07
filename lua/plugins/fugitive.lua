4c461a03dc2982104e9e5953fa23700052fdecc0
Anibal Mendoza <anibalmd32@gmail.com>
2025-06-06 21:40:50 -0400

    add key maps for fugitive plugin

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,11 @@
 return {
-	'tpope/vim-fugitive'
+	'tpope/vim-fugitive',
+	cmd = {'G', 'Git'},
+	keys = {
+		{'<leader>ga', ':Git add .', desc = 'add all changes to the stage area'},
+		{'<leader>gc', ':Git commit', desc = 'create a commit'},
+		{'<leader>gpl', ':Git pull', desc = 'make a pull from remote to origin'},
+		{'<leader>gps', ':Git push', desc = 'make a push from origin to remote'},
+		{'<leader>gs', ':Git status', desc = 'show the current repository status'},
+	},
 }
