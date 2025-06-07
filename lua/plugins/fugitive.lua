66c764a2db065c5fc3e22965f3f0979d514ee966
Anibal Mendoza <anibalmd32@gmail.com>
2025-06-06 21:49:23 -0400

    delete unnecesarie key maps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,10 +2,8 @@ return {
 	'tpope/vim-fugitive',
 	cmd = {'G', 'Git'},
 	keys = {
-		{'<leader>ga', ':Git add .', desc = 'add all changes to the stage area'},
-		{'<leader>gc', ':Git commit', desc = 'create a commit'},
 		{'<leader>gpl', ':Git pull', desc = 'make a pull from remote to origin'},
 		{'<leader>gps', ':Git push', desc = 'make a push from origin to remote'},
-		{'<leader>gs', ':Git status', desc = 'show the current repository status'},
+		{'<leader>gs', ':G status', desc = 'show the current repository status'},
 	},
 }
